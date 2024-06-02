a304fad8de736e24d53fa28ea38921a78f7a756d
Dominoid <170596367+Dominoid91260@users.noreply.github.com>
2024-06-02 19:07:50 +1000

    Added primary key to `Settings` model

---
+++ Server/Migrations/20240602090343_SettingsPrimaryKey.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DominosStockOrder.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class SettingsPrimaryKey : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Id",
+                table: "Settings",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0)
+                .Annotation("Sqlite:Autoincrement", true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Settings",
+                table: "Settings",
+                column: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Settings",
+                table: "Settings");
+
+            migrationBuilder.DropColumn(
+                name: "Id",
+                table: "Settings");
+        }
+    }
+}
--- Server/Models/Settings.cs
+++ Server/Models/Settings.cs
@@ -1,10 +1,18 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace DominosStockOrder.Server.Models
 {
-    [Keyless]
     public class Settings
     {
+        /// <summary>
+        /// This is just a dummy property to allow us to use Entity Framework Core
+        /// to write changes to this record. The database should only contain
+        /// a single record for settings.
+        /// </summary>
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
         public int NumFoodTheoWeeks { get; set; }
     }
 }
